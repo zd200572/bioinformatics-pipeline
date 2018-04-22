05d3203380a623aa5d17e6b9314eb5387bb8ba23
zd200572 <zd200572@yahoo.com>
2018-04-21 19:28:00 -0700

    i

--- 16S/sygene/get_Sample_list.py
+++ 16S/sygene/get_Sample_list.py
@@ -2,9 +2,10 @@ import os
 
 fout = open('Samples_list.csv','w')
 fout.write("sample-id,absolute-filepath,direction" + '\n')
-for i in os.listdir('.'):
-	if 'R1.fq.gz' in i:
-		sample_id = i.split(_R1)[0]
-		fout.write(str(sample_id) + ',' + str(os.path.getcwd()) + '/' + sample-id + '_1.fq.gz' +',' + 'forward' + '\n')
-		fout.write(str(sample_id) + ',' + str(os.path.getcwd()) + '/' + sample-id + '_2.fq.gz' +',' + 'reverse' + '\n')
+path = '/home/zjd/16s/test'
+for i in os.listdir(path):
+	if 'R1.fastq.gz' in i:
+		sample_id = i.split('__R1')[0]
+		fout.write(str(sample_id) + ',' + path + '/' + sample_id + '_1.fastq.gz' +',' + 'forward' + '\n')
+		fout.write(str(sample_id) + ',' + path + '/' + sample_id + '_2.fastq.gz' +',' + 'reverse' + '\n')
 fout.close()
